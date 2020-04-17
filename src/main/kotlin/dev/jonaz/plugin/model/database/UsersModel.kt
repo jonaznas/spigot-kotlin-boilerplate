b624991aa27cab5d0af674db7d73a51b86543cc1
Jonas <48187105+jonaznas@users.noreply.github.com>
2020-04-17 20:21:21 +0200

    Update deprecated primary database model key

--- src/main/kotlin/dev/jonaz/plugin/model/database/UsersModel.kt
+++ src/main/kotlin/dev/jonaz/plugin/model/database/UsersModel.kt
@@ -3,10 +3,11 @@ package dev.jonaz.plugin.model.database
 import org.jetbrains.exposed.sql.Table
 
 object UsersModel : Table("users") {
-    val id = integer("id")
-            .autoIncrement()
-            .primaryKey()
+    val id = integer("id").autoIncrement()
+
     val username = text("username")
     val password = text("password")
     val created = text("created")
+
+    override val primaryKey = PrimaryKey(id, name = "id")
 }
